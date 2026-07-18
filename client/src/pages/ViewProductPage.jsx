61704371382c1da8165e9124be04ad1c648373db
Your Name <your_email@example.com>
2026-07-18 14:40:45 +0700

    job

--- client/src/pages/ViewProductPage.jsx
+++ client/src/pages/ViewProductPage.jsx
@@ -1,7 +1,11 @@
+import { useContext } from "react";
 import { useNavigate } from "react-router-dom";
+import { UserContext } from "../contexts/UserContext.jsx";
 
 function ViewProductPage() {
   const navigate = useNavigate();
+  const userData = useContext(UserContext);
+
   return (
     <div>
       <h1>View Product Page</h1>
@@ -11,7 +15,9 @@ function ViewProductPage() {
       </div>
 
       <div className="product-promotion-box">
-        <h2>คุณคือสมาชิกในระดับ (x) ดังนั้นคุณได้สิทธิพิเศษลด 50%</h2>
+        <h2>
+          คุณคือสมาชิกในระดับ {userData.level} ดังนั้นคุณได้สิทธิพิเศษลด 50%
+        </h2>
         <button onClick={() => alert("🥳🥳🥳🥳")}>กดรับสิทธิ</button>
       </div>
 
